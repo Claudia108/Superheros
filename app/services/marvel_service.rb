5cab659deb6fca471878b166284b8b99c425c02c
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2017-05-10 15:39:36 -0600

    Use marvel_api gem

--- app/services/marvel_service.rb
+++ app/services/marvel_service.rb
@@ -1,33 +1,50 @@
 require 'digest/md5'
 
 class MarvelService
-  attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: 'https://gateway.marvel.com/v1/public/')
+    @client = Marvel::Client.new
+
+    @client.configure do |config|
+      config.api_key = ENV['MARVEL_PUBLIC_KEY']
+      config.private_key = ENV['MARVEL_PRIVATE_KEY']
+    end
   end
 
   def characters
-    result
+    @client.characters
   end
 
-  private
+  # I'd like to use Faraday because it is univerally usable
+  # but I get the error "Credentials invalid".
+  # Will continue to build with gem and come back later.
 
-    def result
-      JSON.parse(response.body, symbolize_names: true)[:results]
-    end
 
-    def response(params = {})
-      result = @connection.get "/characters", params.merge(keys)
-      binding.pry
-    end
-
-    def keys
-      ts = DateTime.now
-      hash = Digest::MD5.hexdigest("#{ts}#{ENV['MARVEL_PRIVATE_KEY']}#{'MARVEL_PUBLIC_KEY'}").to_s
-      {ts: ts,
-       apikey: ENV['MARVEL_PUBLIC_KEY'],
-       hash: hash }
-    end
+  # def initialize
+  #   @connection = Faraday.new(url: 'https://gateway.marvel.com/v1/public')
+  # end
+  #
+  # def characters
+  #   result
+  # end
+  #
+  # private
+  #
+  #   def result
+  #     JSON.parse(response.body, symbolize_names: true)[:results]
+  #   end
+  #
+  #   def response(params = {})
+  #     @connection.get "/characters", params.merge(keys)
+  #   end
+  #
+  #   def keys
+  #     ts = SecureRandom.urlsafe_base64
+  #     hash = Digest::MD5.hexdigest("#{ts} #{ENV['MARVEL_PRIVATE_KEY']} #{'MARVEL_PUBLIC_KEY'}").to_s
+  #     # hash = Digest::MD5.new.update("#{ts}#{ENV['MARVEL_PRIVATE_KEY']}#{'MARVEL_PUBLIC_KEY'}").to_s
+  #     {ts: ts,
+  #      apikey: ENV['MARVEL_PUBLIC_KEY'],
+  #      hash: hash }
+  #   end
 
 end
